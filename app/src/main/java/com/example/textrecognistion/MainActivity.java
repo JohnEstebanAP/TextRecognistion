3073ee03a86963ead34651ba3583374bda78cb7f
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2021-11-02 22:39:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/textrecognistion/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.textrecognistion;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Button CaptureImageBtn, detectTextBtn;
+    private ImageView imageView;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       CaptureImageBtn=findViewById(R.id.btn_capture_image);
+       detectTextBtn=findViewById(R.id.btn_detect_text_image);
+       imageView=findViewById(R.id.image_view);
+       textView=findViewById(R.id.text_display);
+
+       CaptureImageBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v){
+
+           }
+       });
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }catch (ActivityNotFoundException e){
+            //display error to the user
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
